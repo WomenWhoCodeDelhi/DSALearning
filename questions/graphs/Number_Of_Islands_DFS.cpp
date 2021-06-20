84405dfd76fbf0b2002994420f95552b6c8a5248
ShrutiAggarwal99 <31125152+ShrutiAggarwal99@users.noreply.github.com>
2021-06-20 15:22:33 +0530

    Create Number_Of_Islands_DFS.cpp

---
+++ questions/graphs/Number_Of_Islands_DFS.cpp
@@ -0,0 +1,41 @@
+// Link to question : https://leetcode.com/problems/number-of-islands/
+
+// DFS Solution
+
+// Homework : Do the same problem using BFS.
+
+class Solution {
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j){
+        int m = grid.size(), n = grid[0].size();
+        // node : grid[i][j]
+        vis[i][j] = 1;
+        vector<int> dx = {0,1,0,-1};
+        vector<int> dy = {1,0,-1,0};
+        for(int k=0;k<4;k++){
+            int i_nbr = i + dx[k], j_nbr = j + dy[k];
+            if(i_nbr >= 0 && i_nbr < m && j_nbr >= 0 && j_nbr < n && grid[i_nbr][j_nbr] == '1' && vis[i_nbr][j_nbr] == 0){
+                dfs(grid,vis,i_nbr,j_nbr);
+            }
+        }
+    }
+    
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int> > vis(m,vector<int>(n,0));
+        int components = 0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == '1' && vis[i][j] == 0) {
+                    components++;
+                    dfs(grid, vis, i, j);
+                }
+            }
+        }
+        return components;
+    }
+};
+
+// every cell grid[i][j] is equivalent to a graph node
+// if node value = 1 : island
+// otherwise ignore
